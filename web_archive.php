319ce41876ce1695e4cd6de69be782757a8f2f49
Francisco Torron <Torronf@yahoo.com>
2023-05-24 10:46:03 -0400

    Permite ver y navegar en Web.archive.org
    Permite ver y navegar en Web.archive.org la pagina buscada, muy util para paginas protegidas con CLOUDFLARE y para analisis de paginas con DEBUG STOP activo.

--- web_archive.php
+++ web_archive.php
@@ -19,12 +19,74 @@ error_reporting(E_ALL);
 ignore_user_abort(true);
 clearstatcache();
 header("X-Robots-Tag: noindex, nofollow", true);
-header("Content-Type: text/plain");	
+header("Content-Type: text/plain"); //Anular esta Linea para Navegacion HTML
 
 ob_start();
 
-function web_archive($url){
+function cUrlGetData($url,$headers=null,$head = null,$getRemoteFilesize = null,$postFields = null,$proxies = null,$cookie = null) {
+$time_out=2222;
+				$ch = curl_init();
 
+				curl_setopt($ch, CURLOPT_URL, $url);
+				if ($postFields && !empty($postFields))
+				{
+					$postfields=rtrim($postFields);
+					curl_setopt($ch, CURLOPT_POST, 1);
+					curl_setopt($ch, CURLOPT_POSTFIELDS,$postfields);
+				}   
+				if ($headers && !empty($headers)) 
+				{
+					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);					
+				}				
+				if ($proxies && !empty($proxies)) 
+				{
+					curl_setopt($url, CURLOPT_PROXY, $proxies);
+					curl_setopt($url, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+					curl_setopt($url, CURLOPT_HTTPPROXYTUNNEL, 0); 
+				}
+				if ($cookie && !empty($cookie)) 
+				{
+					curl_setopt($ch, CURLOPT_COOKIESESSION, 1);
+					curl_setopt($ch, CURLOPT_COOKIEFILE, "./");
+					curl_setopt($ch, CURLOPT_COOKIEJAR, "./"); 
+				}
+				if ($head && !empty($head)) 
+				{
+					curl_setopt($ch, CURLOPT_HEADER, $head);
+					curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
+			$head_data = curl_getinfo($ch, CURLINFO_HEADER_OUT);
+				}
+				if ($getRemoteFilesize && !empty($getRemoteFilesize)) 
+				{
+					curl_setopt($ch, CURLOPT_VERBOSE, 1);
+					curl_setopt($ch, CURLOPT_TIMEOUT, $time_out);
+					curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+					curl_setopt($ch, CURLOPT_NOBODY, 1);
+			$size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+				}			
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($ch, CURLOPT_ENCODING , '');
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+				//curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+				curl_setopt($ch, CURLOPT_TIMEOUT, $time_out);
+				$data = curl_exec($ch);
+    
+    if (curl_errno($ch)) 
+    {
+        echo 'Error:' . curl_error($ch);
+    }
+
+    curl_close($ch);
+	
+	if ($data) return $data;else return FALSE;
+}
+
+function web_archive($url,$update=null){
+
+	if ($update == 1){echo file_get_contents('https://web.archive.org/cdx/save/cdx?url='.$url);}
+	
 	$headers = array(
 	   'authority: web.archive.org',
 	   'upgrade-insecure-requests: 1',
@@ -43,6 +105,6 @@ function web_archive($url){
 	}
 }
 
-echo web_archive('https://m3ukodi.com/site/');
+echo web_archive('https://www3.animeflv.net',$update=1); //(1=Update Null= Nada)
 
 ?>
